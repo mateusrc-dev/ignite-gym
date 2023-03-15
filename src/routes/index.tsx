b202a2cdbdc6c252ff9aafdc35dc0e051921c64a
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-15 10:35:33 -0300

    modify theme in component NavigationContainer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
+import { useTheme, Box } from "native-base";
 
 export function Routes() {
+  const { colors } = useTheme();
+  const theme = DefaultTheme; // we can override the theme of 'NavigationContainer'
+  theme.colors.background = colors.gray[700];
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   );
 }
 
 // creating a context navigation - context of auth routes and application routes
+// we let's use of Box for not to occur glitch in screen
