299ff461f86a5b775a62a7062b7fbc85cd67241b
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-03-16 10:02:08 -0300

    create component HomeHeader

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -10,6 +10,7 @@ import HomeSvg from "@assets/home.svg";
 import HistorySvg from "@assets/history.svg";
 import ProfileSvg from "@assets/profile.svg";
 import { useTheme } from "native-base";
+import { Platform } from "react-native";
 
 type AppRoutes = {
   home: undefined;
@@ -35,28 +36,47 @@ export function AppRoutes() {
         tabBarShowLabel: false, // to delete menu subtitle
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96, // 'Platform' says which system the application is running on
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
       }}
     >
       <Screen
         name="home"
         component={Home}
         options={{
-          tabBarIcon: ({ color }) => ( // we can rescue the default color used by the icons in the application
-            <HomeSvg fill={color} width={iconSize} height={iconSize} />
-          ),
+          tabBarIcon: (
+            { color } // we can rescue the default color used by the icons in the application
+          ) => <HomeSvg fill={color} width={iconSize} height={iconSize} />,
         }}
       />
-      <Screen name="history" component={History} options={{
+      <Screen
+        name="history"
+        component={History}
+        options={{
           tabBarIcon: ({ color }) => (
             <HistorySvg fill={color} width={iconSize} height={iconSize} />
           ),
-        }} />
-      <Screen name="profile" component={Profile} options={{
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
           tabBarIcon: ({ color }) => (
             <ProfileSvg fill={color} width={iconSize} height={iconSize} />
           ),
-        }} />
-      <Screen name="exercise" component={Exercise} />
+        }}
+      />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ tabBarButton: () => null }} // this function return void because this option is hidden in screen
+      />
     </Navigator>
   );
 }
