9c2a26f0ea64fbf43835870c1168e4286ccdaffc
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-14 10:13:43 -0300

    updated project

--- App.tsx
+++ App.tsx
@@ -9,6 +9,9 @@ import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
 import { Routes } from "./src/routes";
 import { AuthContextProvider } from "@contexts/AuthContext"; // we let's to share data of user with the context 'AuthContext'
+import OneSignal from "react-native-onesignal";
+
+OneSignal.setAppId("5deef90f-4809-47b1-90d7-65f52a99174d");
 
 export default function App() {
   const [fontsLoaded] = useFonts({
